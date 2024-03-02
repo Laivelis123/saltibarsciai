93607bdacc80c44d4c8b8fbbf73f63e6d9060bc8
GhostyJusty <juscec@ktu.lt>
2024-03-02 18:42:24 +0200

    Sukūriau linkus į meniu punktus

---
+++ testo-mokykla/src/Pages/Naujienos.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react'
+import UI from "../components/UI";
+
+function Naujienos() {
+
+    return (
+        <UI>
+        <h2>Kolkas naujienų nėra</h2>
+        </UI>
+    );
+}
+export default Naujienos
