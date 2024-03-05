613b03ce0a50cef4dc408ad5f9a937730d615f2c
jusivo <justinaa9455@gmail.com>
2024-03-05 11:25:51 +0200

    psl_prideta

---
+++ testo-mokykla/frontend/src/Pages/Informaciniai/fizikos/Elektronine.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import UI from "../../../components/UI";
+function Elektronine()
+{
+    return (
+        <UI>
+        {
+            <div className="container"> 
+            <h1 id='header'>
+                Elektroninė vidinė energija
+            </h1>
+            <p1>
+            Elektroninė vidinė energija yra energija, susijusi su elektronų judėjimu ir jų buvimu atomo ar molekulės orbitose. Ši energija apima kelias pagrindines dalis, kurios yra svarbios suprasti atomo struktūrą ir elektroninį elgesį:
+
+Elektroninės orbitos:
+
+Atomo branduolys susideda iš protonų ir neutronų, o elektronai sukaunasi jo aplinkoje, judėdami tam tikromis orbitomis arba energetinėmis lygtimis.
+Kinetinė energija:
+
+Elektronai, judėdami orbitose, turi kinetinę energiją dėl savo judėjimo. Kinetinė energija priklauso nuo elektrono masės ir jo judėjimo greičio.
+Potencinė energija:
+
+Elektronai, buvę skirtingose orbitose, turės skirtingą potencinę energiją. Tai susiję su atstumu nuo branduolio - elektronui būnant arti branduolio, potencinė energija didėja, o atitolus, ji mažėja.
+Elektroninės energinės lygtys:
+
+Elektronai gali užimti tam tikras orbitas arba energetines būsenas. Elektronų energijos lygtys aprašo leistinus elektronų energijos lygius ir padeda prognozuoti, kurie orbitiniai lygmenys gali būti užpildyti elektronais.
+Elektronų pertvarkymasis:
+
+Elektronai gali pertvarkyti savo energiją, kai jie perėja iš vienos orbitos į kitą. Tai gali įvykti, kai elektronas absorbuoja arba išspinduliuoja fotoną.
+Elektroninės spektroskopija:
+
+Elektroninė vidinė energija yra svarbi elektromagnetinės spinduliuotės absorbcijos ir išspinduliavimo tyrimams. Elektroninės spektroskopijos metodai leidžia tyrinėti molekulių elektroninę struktūrą.
+Cheminės savybės:
+
+Elektroninė vidinė energija taip pat turi įtakos atomo cheminėms savybėms. Molekulės susidarymas, cheminiai ryšiai ir reakcijos daugiausia priklauso nuo elektroninės struktūros.
+Kvantinė mechanika:
+
+Elektronų elgesys orbitose aprašomas kvantinės mechanikos principais, o elektroninė vidinė energija susijusi su šiais kvantinės mechanikos aspektais. Tai apima elektronų orbitalių kvantavimą ir juos aprašančius matematinius modelius.
+Supratimas apie elektroninę vidinę energiją yra svarbus fizikos, chemijos ir kitose mokslų srityse, nes tai padeda paaiškinti atomo ir molekulės elgesį.
+                
+                           
+            </p1>
+        
+            </div>
+          
+        }
+        </UI>
+    );
+}
+export default Elektronine;
---
+++ testo-mokykla/frontend/src/Pages/Informaciniai/fizikos/kieti.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import UI from "../../../components/UI";
+function kieti (){
+    return (
+        <UI>
+        {
+            <div className="container"> 
+            <h1 id='header'>
+                Branduolinė vidinė energija
+            </h1>
+            <p1>
+            Branduolinė vidinė energija yra energija, susijusi su atominėmis branduolių sąveikomis. Ši energija yra gausi labai dideliuose atomuose, kurie turi daug protonų ir neutronų branduolyje. Pagrindinės savybės ir informacija apie branduolinę vidinę energiją yra šios:
+
+Branduolinė sąveika:
+
+Branduolinė vidinė energija susijusi su sąveika tarp protonų ir neutronų, esančių atomų branduoliuose. Ši sąveika palaiko atomų struktūrą.
+Atominiai branduoliai:
+
+Dideliuose atomuose, pavyzdžiui, uranu ar plutonu, branduolinė vidinė energija yra ypač svarbi. Tokiuose branduoliuose esantys protonai atstumiami elektromagnetinės atstūmimo jėgos, bet traukiami stipria branduoline jėga.
+Branduolinių reakcijų energija:
+
+Branduolinė vidinė energija gali būti atlaisvinama arba absorbuojama branduolinių reakcijų metu. Ši energija yra atlaisvinama, kai lengvai branduoliai susijungia (branduolinė sintezė) arba kai sunkūs branduoliai suskyla (branduolinis skilimas).
+Branduolinis skilimas:
+
+Branduolinis skilimas yra vienas iš procesų, kurį galima naudoti branduolinės energijos gamybai. Pvz., kai urano ar plutonio branduoliai suskyla, atlaisvinama didžiulė energijos kiekis.
+Branduolinės sintezės:
+
+Branduolinės sintezės metu lengvi branduoliai sujungiami sukuriant sunkesnius branduolius. Šis procesas vyksta, pavyzdžiui, saulėje, kur vandenilis paverčiamas heliumu, ir tai yra šaltinis saulės energijos.
+Branduolinės energijos panaudojimas:
+
+Branduolinė vidinė energija naudojama branduolinėse elektrinėse, kurios generuoja elektros energiją branduolinių reakcijų rezultate. Tai gali būti ir potencialus šaltinis ateities energijos poreikiams tenkinti.
+                <h2>Branduolinės energijos formos</h2>
+                <li>
+                Branduolinė vidinė energija yra energetinė būsena, kuri 
+                susijusi su branduolio sudėtimi ir jo komponentų, tokiais kaip kvarkai ir gluonai,
+                sąveika. Ši energija yra susijusi su stipraus branduolinio sąveikos jėga, kuri veikia tarp 
+                kvarkų ir gluonų.                </li>
+            </p1>
+        
+            </div>
+        }
+        </UI>
+    );
+}
+export default  kieti;    
