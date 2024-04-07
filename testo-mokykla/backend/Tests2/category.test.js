838d6d0f28f911c173a4e33c4814d4530b133072
jusivo <justinaa9455@gmail.com>
2024-04-07 17:31:16 +0300

    papildymas

--- testo-mokykla/backend/Tests2/category.test.js
+++ testo-mokykla/backend/Tests2/category.test.js
@@ -1,5 +1,5 @@
 const router  = require('../routes/category'); // Replace with the actual path to your router file
-const { Category } = require('../models');
+const { User, Category } = require('../models');
 const { Op } = require('sequelize');
 const { json } = require('body-parser');
 const supertest = require('supertest');
@@ -7,11 +7,9 @@ const express = require('express');
 const app = express();
 // Gauna visus kategorijas pagal filtrą arba be filtro.
 app.use(router);
-
-
 describe('filtering_categories_filter', () =>{
     
-    it('su filtru: ', async () => {
+    it('be filtro: ', async () => {
         const res = await supertest(app).get('/filter');
           //await router(req,res);   
           expect(res.statusCode).toBe(200);
@@ -19,7 +17,53 @@ describe('filtering_categories_filter', () =>{
           //expect(res.data).
 
     });
+    it('su filtru', async () => {
+        const res = await supertest(app).get('/filter?search=test');
+        expect(res.statusCode).toBe(200);  
+
+    });
     
+});
+
+//// Gauna visas kategorijas.
+describe('visos kategorijos', () => {
+    it('korektiskai visos', async () => {
+        const result = await supertest(app).get('/all');
+        expect(result.statusCode).toBe(200);
+
+    });
+
+});
+// Gauna kategorijos vaikines kategorijas pagal nurodytą ID.(PATAISYTI!!)
+/*describe('vaikines visos', () => {
+    it('visos', async () => {
+        const mocking = [{ id: 1, name: 'kategorija 1' }];
+        jest.spyOn(Category, 'findAll').mockResolvedValue(mocking);
+        const res = await supertest(app).get('/1/children');
+        expect(res.statusCode).toBe(200);
+
+    });
+});
+*/
+
+// Sukuria naują kategoriją, TESTAVIMAS:.
+describe('kategorijos kurimas:', () => {
+    it('sukurta kategorija', async () => {
+        //const result = await supertest(app).get('/create');
+        const categoryData = {
+            name: '',
+            bulletPoints: '',
+            parentId: null
+        };   
+        //
+        const res = await supertest(app).post( '/create').send(categoryData);
+        expect(res.statusCode).toBe(201);
+
+    });
+
+});
+
+// Gauna kategoriją pagal nurodytą ID.
 
 
-})
+
