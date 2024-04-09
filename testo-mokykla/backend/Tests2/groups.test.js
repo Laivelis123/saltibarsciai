e2990a45293d7e15001f5c086f70d8895a04e435
jusivo <justinaa9455@gmail.com>
2024-04-09 15:03:25 +0300

    groups_test pakoreguota

--- testo-mokykla/backend/Tests2/groups.test.js
+++ testo-mokykla/backend/Tests2/groups.test.js
@@ -8,17 +8,21 @@ const app = express();
 app.use(express.json());
 app.use('/', router); 
 //token_patikrinimui:
-
 //sukuriama grupe su prisijungimo kodu:
 describe('post /create', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+      });
+      
+      
     it('sukurta nauja grupe: ', async () => {
         const req = {name: 'testing'};
         Group.create = jest.fn().mockResolvedValue({
 
             id: 'mockGroupId',
             name: req.name,
-            code: 'mockCode',
-            userId: 'mockUserId'
+            //code: 'mockCode',
+            //userId: 'mockUserId'
         });
         const response = await supertest(app)
       .post('/create')
@@ -40,4 +44,68 @@ describe('post /create', () => {
 
 
 });
+
+// Vartotojas prisijungia prie grupės
+describe('POST /join', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+      });
+      it('sekmingai prisijungia i grupe:', async () => {
+        const code = 'validCode';
+        const token = 'validToken';
+        const userId = 'userId';
+        jwt.verify.mockResolvedValueOnce({})
+        const res = await request(app)
+        .post('/join')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ code });
+
+        expect(res.statusCode).toBe(201);
+        expect(res.body.message).toBe('Sėkmingai prisijungta prie grupės');
+
+      });
+      it('error handling: ', async () => {
+        const code = 'invalidCd';
+        const token = 'invalid';
+        jwt.verify.mockImplementation(() => {
+            throw new Error('Invalid token');
+          });
+        const res = await request(app)
+        .post('/join')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ code });
+        expect(res.statusCode).toBe(500);
+        expect(res.body.error).toBe('Vidinė serverio klaida');
+
+      });
+    
+});
+// Gaunami vartotojo grupės
+describe('GET /my-groups', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+      });
+      beforeAll(() => {
+        process.env.JWT_SECRET = 'da63c645807941e8b65af7271caca6af17ed20edd40cbdd030618a2b9596dc5b';
+    });
+    it('valid token extracted all groups', async () => {
+        const userId = 'userId';
+        jest.spyOn(jwt, 'verify').mockReturnValueOnce({ id: userId });       
+         const token = jwt.sign({ id: userId }, process.env.JWT_SECRET);
+        const response = await supertest(app)
+        .get('/my-groups')
+        .set('Authorization', `Bearer ${token}`);
+        expect(response.status).toBe(200);
+    });
+    it('500 if errors', async () => {
+        jest.spyOn(jwt, 'verify').mockImplementation(() => {
+            throw new Error('Invalid token');
+        });
+        const response = await supertest(app)
+        .get('/my-groups');
+        expect(response.status).toBe(500);
+    });
+  
+});
+
 
